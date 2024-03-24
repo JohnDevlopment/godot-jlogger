12157949a488514ed3e58157fbe144333db71c67
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-03-24 07:25:50 -0500

    Logging: Implement get_logger()

--- addons/logging/logging_autoload.gd
+++ addons/logging/logging_autoload.gd
@@ -3,10 +3,32 @@
 extends Node
 
 var _logger: Logger
+var _cache := {}
 
 func _ready() -> void:
 	if not Engine.is_editor_hint():
-		_logger = Logger.new("root")
+		_logger = get_logger()
+
+## Returns a [Logger] with the given [param name] and [param level].
+## Loggers returned from this function are cached using [param name]
+## as the key. So, subsequent calls to this function with the same name
+## will return the same exact logger.
+func get_logger(name: StringName = &"", 
+level: Logger.Level = Logger.Level.DEFAULT) -> Logger:
+	# Root logger if name == "root" or ""
+	if name == &"":
+		name = &"root"
+	
+	# Return cached logger, if defined
+	if name in _cache:
+		return _cache[name]
+	
+	# Otherwise, create new logger and add to cache
+	var logger := Logger.new(name, level)
+	if OS.is_debug_build():
+		print_debug("Creating logger: ", name)
+	_cache[name] = logger
+	return logger
 
 ## Prints a log with the severity debug.
 func debug(fmt: String, args: Array = []) -> void:
