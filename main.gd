545953906894fb814c2eb4627bc56db65752dc7e
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-03-24 05:03:00 -0500

    Update main.gd

--- main.gd
+++ main.gd
@@ -1,9 +1,23 @@
 extends CenterContainer
 
+var some_var := 1
+
 func _ready() -> void:
+	_logging_function_1()
+
+func _logging_function_1() -> void:
 	var logger := Logger.new("test", Logger.Level.DEBUG)
 	logger.debug("This is a log.")
 	logger.info("This is a log.")
 	logger.warning("This is a log.")
 	logger.error("This is a log.")
-	logger.critical("This is a log.")
+	if ProjectSettings.get_setting_with_override("application/run/project/enable_critical"):
+		logger.critical("This is a log.")
+
+func _logging_function_2() -> void:
+	Logging.debug("This is a log.")
+	Logging.info("This is a log.")
+	Logging.warning("This is a log.")
+	Logging.error("This is a log.")
+	if ProjectSettings.get_setting_with_override("application/run/project/enable_critical"):
+		Logging.critical("This is a log.")
