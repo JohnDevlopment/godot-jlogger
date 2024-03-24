015112cb0d989b77627c9736711e249dbe671b33
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-03-24 06:40:59 -0500

    Plugin: Load LoggingConfig before Logging

--- addons/logging/logging.gd
+++ addons/logging/logging.gd
@@ -2,8 +2,8 @@
 extends EditorPlugin
 
 func _enter_tree() -> void:
-	add_autoload_singleton("Logging", "res://addons/logging/logging_autoload.gd")
 	add_autoload_singleton("LoggingConfig", "res://addons/logging/LoggingConfig.gd")
+	add_autoload_singleton("Logging", "res://addons/logging/logging_autoload.gd")
 
 func _exit_tree() -> void:
 	remove_autoload_singleton("Logging")
