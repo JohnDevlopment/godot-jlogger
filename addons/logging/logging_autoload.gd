16b67fa9a0bca25a2a6158a4a844e711bb916471
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-03-24 05:05:03 -0500

    Logging: Add logging functions

--- addons/logging/logging_autoload.gd
+++ addons/logging/logging_autoload.gd
@@ -1,9 +1,35 @@
-## Class that handles all the logging in this add-on.
-## This can be used like a singleton, or a 
+## A singleton that can be used for logging purposes.
 @tool
 extends Node
 
-## Individiual [code]Logger[/code] class
-const Logger := preload("res://addons/logging/logger.gd")
-
 const Settings := preload("res://addons/logging/settings.gd")
+
+var _logger: Logger
+
+func _ready() -> void:
+	_logger = Logger.new("root", LoggingConfig.logger_level)
+
+## Prints a log with the severity debug.
+func debug(fmt: String, args: Array = []) -> void:
+	assert(is_instance_valid(_logger))
+	_logger.debug(fmt, args)
+
+## Prints a log with the severity info.
+func info(fmt: String, args: Array = []) -> void:
+	assert(is_instance_valid(_logger))
+	_logger.info(fmt, args)
+
+## Prints a log with the severity warning.
+func warning(fmt: String, args: Array = []) -> void:
+	assert(is_instance_valid(_logger))
+	_logger.warning(fmt, args)
+
+## Prints a log with the severity error.
+func error(fmt: String, args: Array = []) -> void:
+	assert(is_instance_valid(_logger))
+	_logger.error(fmt, args)
+
+## Prints a log with the severity critical.
+func critical(fmt: String, args: Array = []) -> void:
+	assert(is_instance_valid(_logger))
+	_logger.critical(fmt, args)
